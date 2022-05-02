95d24f7e17d4220a2bb3eac3234273ea00eb9267
tajerek <tajerek@gmail.com>
2022-05-02 01:06:52 +0100

    Add files via upload

--- aparser.py
+++ aparser.py
@@ -8,9 +8,7 @@ import streamlit as st
 import pandas as pd
 
 ###################################
-from st_aggrid import AgGrid
-from st_aggrid.grid_options_builder import GridOptionsBuilder
-from st_aggrid.shared import JsCode
+
 
 ###################################
 
